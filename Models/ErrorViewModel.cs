2a3701ab48527b08e11a3139a09264f4de42ef3b
Celikel <ekarlibas@outlook.com>
2022-04-03 16:43:55 +0300

    project added

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesReporting.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
